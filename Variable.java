74b2e125e0818e87e7883ba64276520031ada9e2
priyambadasahoo2007-a11y <priyambadasahoo2007@gmail.com>
2026-07-21 09:32:56 +0530

    Day 2: Practiced Java variables

---
+++ Variable.java
@@ -0,0 +1,15 @@
+
+public class Variable
+ {
+
+    public static void main(String[] args) {
+        int quantity = 5;
+        double itemPrice = 24.66;
+        // Output Value
+        System.out.println("Quantity:" +quantity);
+        System.out.println("Price:" +itemPrice);
+
+
+    
+    }
+}
