bd489dcd8942fceaf579728abe1a9d29a5e34467
priyambadasahoo2007-a11y <priyambadasahoo2007@gmail.com>
2026-07-22 10:12:13 +0530

    Day 3: Added Java data types

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        char enemy = '*';
+        boolean isLoggedIS = true;
+        String coddy = " learn with coody ";
+
+        // output
+        System.out.println("Enemy :" + enemy);
+        System.out.println("Successful Login :" + isLoggedIS);
+        System.out.println("Coddy :" + coddy);
+    }
+}
