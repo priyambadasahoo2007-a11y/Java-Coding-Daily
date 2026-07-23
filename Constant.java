5805ac4f2a142b27e7ee5c0f7c94c71573b8dd05
priyambadasahoo2007-a11y <priyambadasahoo2007@gmail.com>
2026-07-23 10:18:38 +0530

    Day 4: Added Java Constant

---
+++ Constant.java
@@ -0,0 +1,13 @@
+
+public class Constant {
+
+    public static void main(String[] args) {
+        final int k = 88;
+        final double decimal = 3.14159;
+        final String user = " bob ";
+
+        System.out.println("K=" + k);
+        System.out.println("Decimal =" + decimal);
+        System.out.println("User =" + user);
+    }
+}
