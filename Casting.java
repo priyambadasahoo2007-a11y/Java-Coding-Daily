504f4cc4ddd72ddaf87fc97d25dea709f45b9b61
priyambadasahoo2007-a11y <priyambadasahoo2007@gmail.com>
2026-07-24 19:21:12 +0530

    Day 5: Added Java data types

---
+++ Casting.java
@@ -0,0 +1,15 @@
+
+public class Casting {
+
+    public static void main(String[] args) {
+        //Explicit
+
+        double price = 19.4;
+        int roundPrice = (int) price;
+        //Output
+        
+        System.out.println("price : " + price);
+        System.out.println("RoundPrice : " + roundPrice);
+
+    }
+}
