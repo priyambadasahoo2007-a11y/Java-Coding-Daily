a1b8917f3d1df2102b569508480bb8154206bc9b
priyambadasahoo2007-a11y <priyambadasahoo2007@gmail.com>
2026-07-26 19:55:23 +0530

    Added increment and decrement operators

---
+++ IncrementDecrement.java
@@ -0,0 +1,12 @@
+
+public class IncrementDecrement {
+
+    public static void main(String[] args) {
+        int count = 0;
+        count = count + 4;
+        count = count * 2;
+        count = count - 1;
+        //Output
+        System.out.println("Count : " + count);
+    }
+}
