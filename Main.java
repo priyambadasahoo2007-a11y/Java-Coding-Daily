c69650d7f5cdf499ada324a11af67fce42ce5cc5
priyambadasahoo2007-a11y <priyambadasahoo2007@gmail.com>
2026-07-31 09:23:20 +0530

    Add if Statement

--- Main.java
+++ Main.java
@@ -2,13 +2,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        char enemy = '*';
-        boolean isLoggedIS = true;
-        String coddy = " learn with coody ";
+        // Initialize your values here
+        int score = 92;
+        int bonus = 10;
 
-        // output
-        System.out.println("Enemy :" + enemy);
-        System.out.println("Successful Login :" + isLoggedIS);
-        System.out.println("Coddy :" + coddy);
+        // Don't modify the code below
+        String status = "Regular Score";
+
+        if (score >= 80 && score <= 100 && bonus >= 10 && bonus <= 20) {
+            if ((score > 90 && bonus > 15) || (score > 85 && bonus > 18)) {
+                status = "High Score";
+            }
+        } else {
+            status = "Invalid Values";
+        }
+
+        System.out.println("Status: " + status);
     }
 }
