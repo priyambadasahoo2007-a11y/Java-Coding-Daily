867eeeab5e45258811a2898aa9ff26285e45e7ca
priyambadasahoo2007-a11y <priyambadasahoo2007@gmail.com>
2026-08-06 09:22:11 +0530

    Nested If

---
+++ SimpleProgram.java
@@ -0,0 +1,13 @@
+/*1.Print "Hello, " and "Coddy!" on the same line.
+2.Print "Java is fun!" on a new line using println.
+ */
+public class SimpleProgram {
+
+    public static void main(String[] args) {
+        System.out.print("Hello, ");
+        // Write your code below
+        System.out.println("Coddy!");
+        System.out.println("Java is fun!");
+
+    }
+}
