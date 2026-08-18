5897540071336537f53ea4503c2a7dbd0492cc25
priyambadasahoo2007-a11y <priyambadasahoo2007@gmail.com>
2026-08-18 10:30:33 +0530

    Add ForStatement

---
+++ ForStatement.java
@@ -0,0 +1,18 @@
+/* Print Hello Coddy: 3
+         Hello Coddy: 4
+         .
+         .
+         .
+         .
+         .
+         Hello Coddy: 27 */
+
+public class ForStatement {
+
+    public static void main(String[] args) 
+    {
+        for (int i = 3; i <= 27; i++) {
+            System.out.println("Hello Coddy: " + i);
+        }
+    }
+}
