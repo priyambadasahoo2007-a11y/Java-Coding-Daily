a638b6dff19939fca3ec84e1c192aee7eff29a7d
priyambadasahoo2007-a11y <priyambadasahoo2007@gmail.com>
2026-08-26 20:34:51 +0530

    Add Parameter Program

---
+++ Parameters.java
@@ -0,0 +1,12 @@
+
+class Parameters {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = add(10, 20);
+        System.out.println(result);
+    }
+}
