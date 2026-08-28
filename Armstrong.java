a4fd0f86a71c1c2046aa76bf27d03371a7074f58
priyambadasahoo2007-a11y <priyambadasahoo2007@gmail.com>
2026-08-28 16:33:05 +0530

    Add Armstrong

---
+++ Armstrong.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Armstrong {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int original = n;
+        int sum = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            sum += digit * digit * digit;
+            n /= 10;
+        }
+
+        System.out.println(original == sum ? "Armstrong" : "Not Armstrong");
+    }
+}
