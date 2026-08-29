8f0901dea56a1cc0cdc81b19eb2ba935689e440f
priyambadasahoo2007-a11y <priyambadasahoo2007@gmail.com>
2026-08-29 22:06:26 +0530

    Add Palindrome

---
+++ palindrome.java
@@ -0,0 +1,30 @@
+
+public class palindrome {
+
+    public static void main(String[] args) {
+        int number = 121;
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(int num) {
+        if (num < 0) {
+            return false;
+             }
+
+        int originalNumber = num;
+        int reversedNumber = 0;
+
+        while (num != 0) {
+            int lastDigit = num % 10;
+            reversedNumber = (reversedNumber * 10) + lastDigit;
+            num = num / 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+}
