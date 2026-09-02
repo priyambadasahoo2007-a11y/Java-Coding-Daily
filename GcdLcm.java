3d595bdab0e018fba64a4fbdd5d43fe14fc54ac1
priyambadasahoo2007-a11y <priyambadasahoo2007@gmail.com>
2026-09-02 10:39:33 +0530

    Add GCD LCM program

---
+++ GcdLcm.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class GcdLcm {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int x = a;
+        int y = b;
+
+        // Find GCD using Euclidean Algorithm
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+
+        int gcd = x;
+
+        // Formula: LCM = (a × b) / GCD
+        int lcm = (a * b) / gcd;
+
+        System.out.println("GCD = " + gcd);
+        System.out.println("LCM = " + lcm);
+
+        sc.close();
+    }
+}
